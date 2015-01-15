78e6e6636d512a99292b226b75173cfa17fa4a3e
tomcat123456 <514658996@qq.com>
2015-01-15 22:17:25 +0800

    简介
    。。

---
+++ js/app.js
@@ -0,0 +1,56 @@
+var routerApp = angular.module('routerApp', ['ui.router']);
+/**
+ * 由于整个应用都会和路由打交道，所以这里把$state和$stateParams这两个对象放到$rootScope上，方便其它地方引用和注入。
+ * 这里的run方法只会在angular启动的时候运行一次。
+ * @param  {[type]} $rootScope
+ * @param  {[type]} $state
+ * @param  {[type]} $stateParams
+ * @return {[type]}
+ */
+routerApp.run(function($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+});
+
+/**
+ * 配置路由。
+ * 注意这里采用的是ui-router这个路由，而不是ng原生的路由。
+ * ng原生的路由不能支持嵌套视图，所以这里必须使用ui-router。
+ * @param  {[type]} $stateProvider
+ * @param  {[type]} $urlRouterProvider
+ * @return {[type]}
+ */
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/index');
+    $stateProvider
+        .state('index', {
+            url: '/index',
+            views: {
+                '': {
+                    templateUrl: 'tpls/main.html'
+                }
+            }
+        }).state('literature', {
+            url: '/literature',
+            templateUrl: 'tpls/show.html'
+        }).state('literature.luo', {
+            url: '/luo',
+            templateUrl: 'tpls/luo.html'
+        }).state('literature.wang', {
+            url: '/wang',
+            templateUrl: 'tpls/wang.html'
+        }).state('literature.yang', {
+            url: '/yang',
+            templateUrl: 'tpls/yang.html'
+        }).state('work',{
+            url: '/yang',
+            templateUrl: 'tpls/work.html'
+        }).state('movie',{
+            url: '/movie',
+            templateUrl: 'tpls/movie.html'
+        }).state('move',{
+            url: '/move',
+            templateUrl: 'move.html'
+        })
+        
+});
